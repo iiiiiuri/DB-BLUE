71dfc66e148f43bae6d9590fd93873245be9327f
iiiiiuri <iurilima.395@gmail.com>
2023-06-16 10:29:45 -0300

    Re-Commit
    Using the Django framework documentation, I found another way to navigate between the pages 'Home>Login' 'Login>Home' and I implement that.

--- DBBLUE/urls.py
+++ DBBLUE/urls.py
@@ -22,5 +22,6 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('', views.home, name='home')
 ]
