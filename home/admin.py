aa424250d62bd861c0ea0f3a17285177dcad6373
iiiiiuri <iurilima.395@gmail.com>
2023-06-16 11:43:53 -0300

    Making Users
    The first users, tables and accounts were created to test the application

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import contas
+from .models import dados
 # Register your models here.
+
+
+admin.site.register(contas)
+admin.site.register(dados)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class contas(models.Model):
+    nome = models.CharField(max_length=15)
+    sobrenome = models.CharField(max_length=15)
+    user = models.CharField(max_length=10)
+    senha = models.CharField(max_length=25)
+    email = models.EmailField(max_length=25)
+
+
+class dados(models.Model):
+    usuario = models.ForeignKey(contas, on_delete=models.CASCADE)
+    dados = models.FileField(upload_to='Arquivos', max_length=100)
