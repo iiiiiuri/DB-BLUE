93e131b7c50aca9eebc8a257efdd27893723e644
iiiiiuri <iurilima.395@gmail.com>
2023-06-20 11:38:09 -0300

    Mudança Botao Criar Conta
    O botão para criar conta, estava descrito como um link, foi mudado para um input

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import contas
 
 def home(request):
 
@@ -11,4 +12,11 @@ def login(request):
     return render(request, 'login\login.html')
 
 def signup(request):
+    cadastro = contas()
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    senha = request.POST.get('pass')
+    usuario = request.POST.get('user')
+    email = request.POST.get('email')
+    cadastro.save()
     return render (request, 'signup\signup.html')
